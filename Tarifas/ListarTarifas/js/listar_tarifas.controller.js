01d8af1ca0c85df2d73246c3aeaeff635690f637
aosante <37124700+aosante@users.noreply.github.com>
2018-03-23 07:00:55 -0600

    Andres - Eliminar tarifas

--- Tarifas/ListarTarifas/js/listar_tarifas.controller.js
+++ Tarifas/ListarTarifas/js/listar_tarifas.controller.js
@@ -12,6 +12,7 @@ function mostrarTarifas() {
     tbody.innerHTML = '';
 
     for(let i = 0; i < listaTarifas.length; i++) {
+        if(listaTarifas[i][5] == 1){
         if(listaTarifas[i][0].includes(sFiltro)) {
             let fila = tbody.insertRow();
 
@@ -43,12 +44,15 @@ function mostrarTarifas() {
 
             let botonDesactivar = document.createElement('i');
             botonDesactivar.classList.add("fas", "fa-times");
-            botonDesactivar.dataset.sucursal = listaTarifas[i][0];
+            botonDesactivar.dataset.indice = i;
+            botonDesactivar.addEventListener('click', eliminar);
             cDesactivar.appendChild(botonDesactivar);
 
 
-        }
-    }
+
+        }//if filtro
+       }//if estado
+    }//for
 }
 
 //funcion que redirige a modificar
@@ -61,3 +65,34 @@ function redirect() {
 //para desplegar las tarifas en la tabla, es necesario saber la posicion
 //del nombre de la sucursal, y de las tarifas en el arreglo
 //*buscar con el developer tool y el LS en chrome
+
+function eliminar() {
+        var id = this.dataset.indice;
+        swal({
+            title: "¿Está seguro de eliminar la tarifa?",
+            text: "Si lo hace, la misma desaparecerá por completo",
+            icon: "warning",
+            buttons: {
+              catch: {
+                  text: 'Eliminar',
+                  value: 'Eliminar',
+                  className: 'button',
+              },
+              cancel: 'Cancelar'
+            },
+          })
+          .then((botonUsuario) => {
+              if(botonUsuario === "Eliminar") {
+                  let listaTarifas = obtenerDatoLocal('RegistroLS');
+                  console.log(listaTarifas[id]);
+                  if(listaTarifas[id][5] == '1') {
+                      listaTarifas[id][5] = '0'
+                  }
+                  else {listaTarifas[id][5] = '1'}
+                  actualizarTarifa(listaTarifas[id]);
+                  mostrarTarifas();
+              }
+          })
+    
+    
+    }
